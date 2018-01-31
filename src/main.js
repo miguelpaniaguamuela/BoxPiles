6d5839778c4f8f4e5dad1b4a09a728b79e77b899
MiguelPaniagua <miguelpaniaguamuela@gmail.com>
2018-01-31 18:12:28 +0100

    Estructure

--- src/main.js
+++ src/main.js
@@ -2,56 +2,22 @@ import 'pixi'
 import 'p2'
 import Phaser from 'phaser'
 
-import BootState from './states/Boot'
-import SplashState from './states/Splash'
-import GameState from './states/Game'
+import MenuState from './states/Menu'
+import PreloadState from './states/Preload'
+import PlayState from './states/Game'
 
 import config from './config'
 
 class Game extends Phaser.Game {
   constructor () {
-    const docElement = document.documentElement
-    const width = docElement.clientWidth > config.gameWidth ? config.gameWidth : docElement.clientWidth
-    const height = docElement.clientHeight > config.gameHeight ? config.gameHeight : docElement.clientHeight
+    super(window.innerWidth, window.innerHeight, Phaser.AUTO);
+    this.scaleFactor = {x: window.innerWidth/1920, y:window.innerHeight/1080}; 
+    this.state.add('Preload', PreloadState, false)
+    this.state.add('Menu', MenuState, false)
+    this.state.add('Game', PlayState, false)
 
-    super(width, height, Phaser.CANVAS, 'content', null)
-
-    this.state.add('Boot', BootState, false)
-    this.state.add('Splash', SplashState, false)
-    this.state.add('Game', GameState, false)
-
-    // with Cordova with need to wait that the device is ready so we will call the Boot state in another file
-    if (!window.cordova) {
-      this.state.start('Boot')
-    }
+    this.state.start('Preload')
   }
-}
 
+}
 window.game = new Game()
-
-if (window.cordova) {
-  var app = {
-    initialize: function () {
-      document.addEventListener(
-        'deviceready',
-        this.onDeviceReady.bind(this),
-        false
-      )
-    },
-
-    // deviceready Event Handler
-    //
-    onDeviceReady: function () {
-      this.receivedEvent('deviceready')
-
-      // When the device is ready, start Phaser Boot state.
-      window.game.state.start('Boot')
-    },
-
-    receivedEvent: function (id) {
-      console.log('Received Event: ' + id)
-    }
-  }
-
-  app.initialize()
-}
